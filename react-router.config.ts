7f8df88ef2df6106ff23166f8f96d525d47251be
Sam Joyce <samjoyce@me.com>
2025-05-07 10:57:28 +1000

    chore: disable file-system routing in React Router

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+  appDirectory: false, // Attempt to disable file-system routing
   ssr: true,
   future: {
     unstable_viteEnvironmentApi: true,
